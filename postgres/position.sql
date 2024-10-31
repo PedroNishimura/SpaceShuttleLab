cc8d13427a741d66dee6acb58e3d6197c5092df8
victorganjos <victor.g.a19@outlook.com>
2024-10-30 21:16:31 -0300

    habilitando o processo ponta a ponta pelo jupyter notebook

--- postgres/position.sql
+++ postgres/position.sql
@@ -1,15 +1,30 @@
 CREATE SCHEMA IF NOT EXISTS spacedb;
 
-CREATE TABLE spacedb.posicao (
+CREATE TABLE spacedb.position (
     id SERIAL PRIMARY KEY,
-    hora_info TEXT,
-    letreiro_completo TEXT,
-    cd_linha INTEGER,
-    sentido SMALLINT,
-    letreiro_destino TEXT,
-    letreiro_origem TEXT,
-    quantidade_veiculos INTEGER,
-    prefixo TEXT,
+    hora TEXT,
+    codigo_empresa TEXT,
+    codigo_linha INTEGER,
+    sentido_operacao SMALLINT,
+    destino_linha TEXT,
+    origem_linha TEXT,
+    veiculos_circulacao INTEGER,
+    prefixo_veiculo TEXT,
+    flag_acessivel BOOLEAN,
+    data_hora TIMESTAMP,
+    latitude TEXT,
+    longitude TEXT
+);
+
+CREATE TABLE spacedb.preview (
+    id SERIAL PRIMARY KEY,
+    hora TEXT,
+    codigo_parada TEXT,
+    nome_parada TEXT,
+    latitude_parada TEXT,
+    longitude_parada TEXT,
+    prefixo_veiculo TEXT,
+    previsao_chegada_parada TEXT,
     flag_acessivel BOOLEAN,
     data_hora TIMESTAMP,
     latitude_veiculo TEXT,
